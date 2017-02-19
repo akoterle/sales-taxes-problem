5f18e17cc3d91e91eb1ed37628c4a0ce1e77a2bd
Alessandro Koterle <alessandro.koterle@gmail.com>
2017-02-19 22:31:56 +0100

    ref: details moved to product

--- src/sales-taxes.js
+++ src/sales-taxes.js
@@ -5,26 +5,16 @@ function SalesTaxesService() {
     this.taxCalculator = TaxCalculator();
 }
 
-var isExempt = function (product) {
-    return (product.name.indexOf('book') != -1)
-        || (product.name.indexOf("box of chocolates") != -1);
-}
-
-var isImported = function (product) {
-    return product.name.indexOf('imported') != -1;
-}
-
 SalesTaxesService.prototype.apply = function (item) {
     var taxRate = 0;
-    if (!isExempt(item)) {
+    if (!item.exempt) {
         taxRate += 10;
     }
-    if (isImported(item)) {
+    if (item.imported) {
         taxRate += 5;
     }
     return this.taxCalculator.calculateTax(item.price, taxRate);
 }
-
 
 module.exports = function SalesTaxes() {
     return new SalesTaxesService()
--- tests/sales-taxes-test.js
+++ tests/sales-taxes-test.js
@@ -8,25 +8,25 @@ describe('SaleTaxes', function () {
 
     it("Basic sales tax is applicable at a rate of 10% on all goods", function () {
         const salesTaxes = SaleTaxes();
-        const item = { quantity: 1, name: "music CD", price: 14.99 };
+        const item = { name: "music CD", price: 14.99, imported: false, exempt: false };
         expect(salesTaxes.apply(item)).to.equal(1.50);
     });
 
-    it("Basic tax is not applicable on products that are exempt", function() {
+    it("Basic tax is not applicable on products that are exempt", function () {
         const salesTaxes = SaleTaxes();
-        const item = { quantity: 1, name: "book", price: 12.49 };
+        const item = { name: "book", price: 12.49, imported: false, exempt: true };
         expect(salesTaxes.apply(item)).to.equal(0);
     });
 
-    it("Import duty is an additional sales tax applicable on all imported goods at a rate of 5%", function() {
+    it("Import duty is an additional sales tax applicable on all imported goods at a rate of 5%", function () {
         const salesTaxes = SaleTaxes();
-        const item = { quantity: 1, name: "imported box of chocolates", price: 10 };
+        const item = { name: "box of chocolates", price: 10, imported: true, exempt: true };
         expect(salesTaxes.apply(item)).to.equal(0.5);
     });
 
-    it("Taxes and duties are both applied for non exempt imported products", function() {
+    it("Taxes and duties are both applied for non exempt imported products", function () {
         const salesTaxes = SaleTaxes();
-        const item = { quantity: 1, name: "imported bottle of perfume", price: 27.99 };
+        const item = { name: "bottle of perfume", price: 27.99, imported: true, exempt: false };
         expect(salesTaxes.apply(item)).to.equal(4.2);
     });
 
