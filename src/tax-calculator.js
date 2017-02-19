d528d276c23bebb30c8269651b62e83d754d9d00
Alessandro Koterle <alessandro.koterle@gmail.com>
2017-02-19 20:20:30 +0100

    ref: clean up

--- src/tax-calculator.js
+++ src/tax-calculator.js
@@ -2,29 +2,10 @@ function TaxCalculatorService() {
     this._round_factor = 0.05;
 }
 
-let isExempt = function (product) {
-    return product.name === 'book';
-}
-
-let isValid = function (product) {
-    return (product && Object.keys(product).length > 0);
-}
-
 TaxCalculatorService.prototype.calculateTax = function (price, rate) {
     var tax = (price * rate) / 100;
     return Math.round(tax / this._round_factor) * this._round_factor;
 }
-
-TaxCalculatorService.prototype.calculate = function (product) {
-    if (!isValid(product) || isExempt(product)) {
-        return 0;
-    }
-    var taxes = 0;
-    if (product.name.startsWith('imported'))
-        taxes += calcTax(product.price, this._import_duty_rate, this._round_factor);
-    taxes += calcTax(product.price, this._basic_tax_rate, this._round_factor);
-    return taxes;
-};
 
 module.exports = function TaxCalculator() {
     return new TaxCalculatorService()
